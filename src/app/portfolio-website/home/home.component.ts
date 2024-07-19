d8235cf950dbd1e3dc60b655e6f44ba9637b2072
abrar19 <abm.abrar@gmail.com>
2024-07-19 04:36:17 +0200

    fade animation added onload

--- src/app/portfolio-website/home/home.component.ts
+++ src/app/portfolio-website/home/home.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
 import { CardComponent } from "../card/card.component";
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [NavbarComponent, FooterComponent, CardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({opacity:0}),
+        animate(2000, style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
 
--- src/app/portfolio-website/portfolio-website.module.ts
+++ src/app/portfolio-website/portfolio-website.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PortfolioWebsiteRoutingModule } from './portfolio-website-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PortfolioWebsiteRoutingModule
+    PortfolioWebsiteRoutingModule,
   ]
 })
 export class PortfolioWebsiteModule { }
