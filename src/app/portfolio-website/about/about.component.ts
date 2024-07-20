eaaaf6462042aee41fc1d092de7fddd2adf6ecd7
abrar19 <abm.abrar@gmail.com>
2024-07-21 01:54:58 +0200

    about-page done

--- src/app/portfolio-website/about/about.component.ts
+++ src/app/portfolio-website/about/about.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
+import { CommonModule } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({opacity:0}),
+        animate(2000, style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class AboutComponent {
+  showEducation:boolean = true;
+  showProjects:boolean = true;
 
+  projects = [
+    {name: 'Project 1', description: 'Description of project 1'},
+    {name: 'Project 2', description: 'Description of project 2'},
+    {name: 'Project 3', description: 'Description of project 3'},
+  ];
 }
