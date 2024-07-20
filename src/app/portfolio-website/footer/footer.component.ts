de43823e8e7a27b5170942c2e33ef905d65bc9b1
abrar19 <abm.abrar@gmail.com>
2024-07-20 08:04:43 +0200

    updated nav and footer liknks

--- src/app/portfolio-website/footer/footer.component.ts
+++ src/app/portfolio-website/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  constructor(public router: Router){
 
+  }
+
+  onHome(){
+    window.scrollTo({top: 0, behavior:'smooth'})
+  }
+
+  onContact(){
+    this.router.navigate(['./portfolio-website/contact'])
+  }
+
+  onAbout(){
+    this.router.navigate(['./portfolio-website/about'])
+  }
 }
--- src/app/portfolio-website/navbar/navbar.component.ts
+++ src/app/portfolio-website/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(public router: Router){
 
+  }
+
+  onHome(){
+    this.router.navigate(['./portfolio-website/home'])
+  }
+
+  onServices(){
+    const servicesSelection = document.getElementById('services-section')
+    if(servicesSelection){
+      servicesSelection.scrollIntoView({behavior: 'smooth', block:'start'})
+    }
+  }
+
+  onContact(){
+    this.router.navigate(['./portfolio-website/contact'])
+  }
+
+  onAbout(){
+    this.router.navigate(['./portfolio-website/about'])
+  }
 }
